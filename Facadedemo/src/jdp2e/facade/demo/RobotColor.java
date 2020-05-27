56bd8cdd0fd25afdc088f2fb82ace98802c15b90
¨Pablo¨ <44410170+PabloJG@users.noreply.github.com>
2020-05-27 16:20:48 -0600

    Tarea2

---
+++ Facadedemo/src/jdp2e/facade/demo/RobotColor.java
@@ -0,0 +1,19 @@
+//RobotColor.java
+
+package jdp2e.facade.demo;
+
+public class RobotColor {
+	 public void setDefaultColor()
+	 { 
+		 System.out.println(" This is steel color robot.");
+	 }
+	 
+	 public void setGreenColor()
+	 { 
+		 System.out.println(" This is a green color robot.");
+	 }
+	 
+	 
+	 
+}
+     
